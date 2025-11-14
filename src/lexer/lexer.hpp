238f887505691abdb04ea3882ba09c5528634f84
Soumik15630m <soumik15630m@gmail.com>
2025-11-15 03:05:21 +0530

    skeleton ready for lexer ....uploading in 3..,.2...,1...

---
+++ src/lexer/lexer.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "tokens.hpp"
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class Lexer{
+public:
+    Lexer(const std::string& source);
+    std::vector<Token> tokenize();
+private:
+    std::string src;
+    std::vector<Token> tokens;//token bucket
+    size_t start = 0;//indexing current token
+    size_t current_index = 0;
+    int line = 1;//line number
+    bool had_error = false;
+    static const std::unordered_map<std::string, TokenType> keywords;
+
+    void scan_token();
+    bool is_at_end();
+    char advance();//advancecs the current index
+    char peek();//jst to chk
+    char peek_next();//2 step ahd
+    bool same(char expt);//check with the expected character
+    void skip_whitespaces_and_comments();
+    void string_literal();//reads the entire string until the closing"
+    void number();
+    void identifier();
+    void add_token(TokenType type);
+    void add_token(TokenType type , const std::string& me);
+    void report_error(const std::string& message);
+
+
+};
